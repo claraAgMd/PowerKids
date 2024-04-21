493d532624b2110109c01c08df1a11afd196b5b8
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2024-04-21 04:53:51 -0300

    finishing the forms

--- src/Pages/Chat2/Chat2.jsx
+++ src/Pages/Chat2/Chat2.jsx
@@ -1,22 +1,22 @@
 import Header from "../../components/Header/Header";
-import "./MChat.css";
+import "./Chat2.css";
 
-const MChat = () => {
+const Chat2 = () => {
     return (
         <>
         <Header/>
         <section className="container-mchat">
             <article>
-                <h2>Seu filho consegue brincar de forma correta com brinquedos pequenos (ex. carros ou blocos), sem apenas colocar na boca, remexer no brinquedo ou deixar o brinquedo cair?</h2>
+                <h2>Seu filho olha para você no olho por mais de um segundo ou dois?</h2>
                 <div className="container-buttons">
                     <button className="simm">Sim</button>
                     <button className="naoo">Não</button>
                 </div>
             </article>
-            <img src="menina-pensativa.png" alt="Menina negra cabelo cacheado brincando pensativa" />
+            <img src="mae-com-crianca.png" alt="Mãe com criança no colo" />
         </section>
         </>
     )
 }
 
-export default MChat
+export default Chat2
