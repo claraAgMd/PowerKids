e403fc284cb5334e6c050989810bdd4229434cb0
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2024-04-20 23:51:07 -0300

    asking the first question on the quiz

--- src/routers.jsx
+++ src/routers.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import InitialQuiz from './Pages/InitialQuiz/InitialQuiz'
 import Login from './Pages/Login/Login'
 import Register from './Pages/Register/Register'
+import MChat from './Pages/MChat/MChat'
 
 
 function RouterApp(){
@@ -11,6 +12,7 @@ function RouterApp(){
             <Route path="/quiz" element={<InitialQuiz/>}>Quiz</Route>
             <Route path="/login" element={<Login/>}>Login</Route>
             <Route path="/cadastro" element={<Register/>}>Cadastro</Route>
+            <Route path="/MChat" element={<MChat/>}>MChat</Route>
             </Routes>
         </Router>
     )
